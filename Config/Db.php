ef435cbc74d35ba60bfb7f84f45d9dc0fa592059
crisXand <cristianmendoza009@hotmail.com>
2023-12-03 12:21:16 -0600

    Database Stringify fetch disabled

--- Config/Db.php
+++ Config/Db.php
@@ -4,8 +4,9 @@ class Db{
     public static function getConn(){
         try {
             //code...
-            $conn = new PDO("mysql:dbname=activo_fijo;host=10.254.50.250", "athenadev", "athenadev");
-
+            $conn = new PDO("mysql:dbname=activo_fijo;port=3307;host=localhost", "root", "");
+            $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+            $conn->setAttribute(PDO::ATTR_STRINGIFY_FETCHES, false);
         } catch (\PDOException $e) {
             echo $e->getMessage();
         }
